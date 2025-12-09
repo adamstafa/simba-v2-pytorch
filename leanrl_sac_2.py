fix weight normalization

leanrl_sac_2.py
@@ -306,9 +306,9 @@ if __name__ == "__main__":
         with torch.no_grad():
             # Normalize weights
             # TODO: evaluate the effect of weight normalization
-            # qf1.normalize_weights()
-            # qf2.normalize_weights()
-            # actor.normalize_weights()
+            qf1.normalize_weights()
+            qf2.normalize_weights()
+            actor.normalize_weights()
             # update the target networks
             for param, target_param in zip(qf1.parameters(), qf1_target.parameters()):
                 target_param.data.copy_(args.tau * param.data + (1 - args.tau) * target_param.data)

simba_v2/layers.py
@@ -38,7 +38,9 @@ class HyperDense(nn.Module):
         return self.linear(x)
     
     def normalize_weights(self):
-        self.linear.weight.data /= torch.norm(self.linear.weight.data, p=2.0)
+        eps = 1e-8
+        weight_norm = torch.norm(self.linear.weight.data, p=2, dim=1, keepdim=True)
+        self.linear.weight.data /= weight_norm.clamp(eps)
 
 
 class HyperMLP(nn.Module):