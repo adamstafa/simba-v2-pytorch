d38605c8a62ec2c791883d3a014beef086bc7b6f
Adam Štafa <xstafa@fi.muni.cz>
2025-12-11 11:09:11 +0100

    remove dead code

--- simba_v2/update.py
+++ simba_v2/update.py
@@ -1,49 +1,5 @@
-
 import torch
 import torch.nn.functional as F
-
-from simba_v2.networks import SimbaV2Actor, SimbaV2Critic
-
-EPS = 1e-8
-
-
-def l2normalize_layer(tree):
-    pass
-
-
-def l2normalize_network():
-    pass
-
-
-def update_actor(
-        actor: SimbaV2Actor,
-        critic: SimbaV2Critic,
-        batch: dict,  # TODO: specify type
-        temperature: float):
-
-    def actor_loss_fn(actor_params) -> tuple[torch.tensor, dict[str, float]]:
-        dist, _ = actor(observations=batch["observation"])
-
-        actions = dist.sample()
-        log_probs = dist.log_prob(actions)
-
-        # qs: (2, n)
-        qs, q_infos = critic(
-            observations=batch["observation"], actions=actions)
-        q = torch.minimum(qs[0], qs[1])
-        actor_loss = (log_probs * temperature() - q).mean()
-
-        actor_info = {
-            "actor/loss": actor_loss,
-            "actor/entropy": -log_probs.mean(),
-            "actor/mean_action": torch.mean(actions),
-        }
-        return actor_loss, actor_info
-
-    # TODO: compute loss
-    # TODO: apply optimizer
-    # TODO: l2 normalize weights
-
 
 def categorical_td_loss(
     pred_log_probs: torch.tensor,  # (n, num_bins)
@@ -70,10 +26,8 @@ def categorical_td_loss(
     # update indices
     b = (target_bin_values - min_v) / ((max_v - min_v) / (num_bins - 1))
     l = torch.floor(b).long()
-    # l = torch.clamp(l, 0, num_bins - 1)  # TODO: investigate why this happens
     l_mask = F.one_hot(l.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
     u = torch.ceil(b).long()
-    # u = torch.clamp(u, 0, num_bins - 1)  # TODO: investigate why this happens
     u_mask = F.one_hot(u.reshape(-1), num_bins).reshape((-1, num_bins, num_bins))
 
     # target label
@@ -87,4 +41,3 @@ def categorical_td_loss(
     loss = -torch.mean(torch.sum(target_probs * pred_log_probs, dim=1))
 
     return loss
-
